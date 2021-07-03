e18a99e4d392366c8ebcff3a86f0a5add047656d
Djuro Alfirevic <djalfirevic@gmail.com>
2021-07-03 09:40:33 +0200

    Initial project added

---
+++ Aliens/Aliens/AliensApp.swift
@@ -0,0 +1,17 @@
+//
+//  AliensApp.swift
+//  Aliens
+//
+//  Created by Djuro on 7/3/21.
+//
+
+import SwiftUI
+
+@main
+struct AliensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
