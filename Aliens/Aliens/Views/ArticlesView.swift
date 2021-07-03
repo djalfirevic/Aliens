d438a1aea16a13edc89fb8d8c6112126d06fe98e
Djuro Alfirevic <djalfirevic@gmail.com>
2021-07-03 09:48:07 +0200

    ArticlesView added

--- Aliens/Aliens/Views/ArticlesView.swift
+++ Aliens/Aliens/Views/ArticlesView.swift
@@ -6,11 +6,41 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct ArticlesView: View {
+    @StateObject var viewModel = ArticlesViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(viewModel.articles) { article in
+                NavigationLink(destination: WebView(url: article.url)) {
+                    ArticleRow(article: article)
+                }
+            }
+            .navigationTitle("Aliens")
+        }
+        .onAppear {
+            self.viewModel.fetchArticles()
+        }
+    }
+}
+
+struct ArticleRow: View {
+    var article: Article
+
+    var body: some View {
+        HStack(spacing: 40) {
+            WebImage(url: article.imageUrl)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .cornerRadius(8)
+                .shadow(radius: 4)
+                .frame(width: 60, height: 60)
+                .shadow(color: Color.black, radius: 20, x: 0, y: 20)
+            
+            Text(article.title)
+        }
     }
 }
 
